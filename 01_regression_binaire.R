01c5713251cee750e7ca69e595e5178477af8ddd
florazhg <flora.zheng30@gmail.com>
2023-12-24 17:39:19 +0100

    01 improved

--- 01_regression_binaire.R
+++ 01_regression_binaire.R
@@ -3,34 +3,35 @@
 ## Q1 : determiner le meilleur modele possible pour modeliser P(Y|X1,X2)
 
 # lire les donnees simu.txt
-simudf <- read.table("/Users/florazheng/Library/CloudStorage/OneDrive-UniversitéParis1Panthéon-Sorbonne/M1 Ecostats/S1/sondages/Projet/donnees/simu.txt", header=T)
+simudf <- read.table("./data/simu.txt", header=T)
 
 # statistiques descriptives
 head(simudf)
 str(simudf)
 summary(simudf)
 
-# transformer Y en valeur binaire
-simudf$Y <- ifelse(simudf$Y == 1, 1, 0) # remplacer les Y=2 par des 0
-simudf$Y <- as.numeric(simudf$Y)
+# transformer Y en facteur car ne prend des valeurs que dans {1,2}
+simudf$Y <- as.factor(simudf$Y)
 
 # regression logistique
-simudf.logit <- glm(Y ~ X1 + X2, data = simudf, family = binomial(link = "logit"))
-simudf.logit # AIC = 2609
+simudf.logit <- glm(Y ~ X1 + X2, data = simudf, family = binomial())
+# score AIC = 2609
 
-# stepwise AIC
-bestAIC = step(simudf.logit)
-summary(bestAIC)
-
-# critere BIC
+# BIC
 library(MASS)
-simudf.BIC <- stepAIC(simudf.logit, direction = "both", trace = FALSE)
+simudf.BIC <- stepAIC(simudf.logit,direction="both",k=log(dim(bdf)[1]))
+
+simudf.AIC <- stepAIC(simudf.logit,direction="both",k=2)
+
+# regression logistique 2
+simudf.logit2 <- glm(Y ~ X1*X2, data = simudf, family = binomial())
+
 
 
 
 ## Q2 : utiliser le fichier "xsimutest" pour predire 1000 observations de la variable Y du fichier de test
 
-simu.test <- read.table("/Users/florazheng/Library/CloudStorage/OneDrive-UniversitéParis1Panthéon-Sorbonne/M1 Ecostats/S1/sondages/Projet/donnees/xsimutest.txt", header=T)
+simu.test <- read.table("./data/xsimutest.txt", header=T)
 
 
 
